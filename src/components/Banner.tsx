c042655e6b78b58c3355368f057327d694780b2a
lmgaspa <luhmgasparetto@gmail.com>
2025-03-31 12:42:49 -0300

    first commit

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -19,17 +19,16 @@ const Banner: React.FC = () => {
     fetch(`https://music-langchain-ebbd399f0b6a.herokuapp.com/search?q=${encodeURIComponent(query)}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log('🎶 Resultado da API:', data);
-        if (Array.isArray(data.results)) {
-          setResults(data.results.slice(0, 16));
-          setShowResults(true);
-        } else {
-          console.error("Formato inesperado:", data);
-          setResults([]);
-          setShowResults(false);
-        }
+        console.log('🎶 Resultado:', data);
+        setResults(data.results || []);
+        setShowResults(true);
       })
-    };      
+      .catch((err) => {
+        console.error('Erro ao buscar:', err);
+        setResults([]);
+        setShowResults(false);
+      });
+  };
 
   return (
     <>
